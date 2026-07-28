e763f2dbafc435f2f3de6729c5a1c70f0670d908
MehdiVaysi <mehdivaysi94@gmail.com>
2026-07-28 08:09:08 +0330

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+// همخونه - نسخه اولیه
+
+function showMessage(text) {
+    alert(text);
+}
+
+function login() {
+    showMessage("ورود با موفقیت انجام شد (نسخه آزمایشی)");
+}
+
+function register() {
+    showMessage("ثبت‌نام با موفقیت انجام شد (نسخه آزمایشی)");
+}
+
+function createRoommateAd() {
+    showMessage("آگهی همخانه ثبت شد (نسخه آزمایشی)");
+}
+
+function createHouseAd() {
+    showMessage("آگهی خانه ثبت شد (نسخه آزمایشی)");
+}
+
+function searchAds() {
+    showMessage("جستجو انجام شد (نسخه آزمایشی)");
+}
+
+function sendMessage() {
+    showMessage("پیام ارسال شد (نسخه آزمایشی)");
+}
